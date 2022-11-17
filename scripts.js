08f9b564c58e8ca62eac8ba829a225260d571854
Muhammad Faqih <freemiumlemper300@gmail.com>
2022-11-17 10:32:22 +0700

    Update script for register

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-document.getElementById("loginform").addEventListener("submit",login);
-
 function login(){
     event.preventDefault();
     var password = document.getElementById("upass").value;
@@ -12,5 +10,25 @@ function login(){
     else{
         alert("Login sukses");
         window.location.replace("Landing.html");
+    }
+}
+
+function register(){
+    event.preventDefault();
+    var username = document.getElementById("reg_user").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("reg_pass").value;
+    var telp = document.getElementById("reg_no").value;
+    console.log(username);
+    console.log(email);
+    console.log(password);
+    console.log(telp);
+    
+    if(username.length === 0 && password.length === 0 && email.length === 0 && telp.length === 0){
+        alert("Kurang lengkap");
+    }
+    else{
+        alert("Berhasil teregistrasi");
+        window.location.replace("Login.html");
     }
 }
