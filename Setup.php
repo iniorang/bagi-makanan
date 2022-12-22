fb7c12179e86f8f84224744a8df38426d23dd2b0
irgydaffa <irgytl12@gmail.com>
2022-12-22 11:59:38 +0700

    update

--- Setup.php
+++ Setup.php
@@ -6,7 +6,7 @@ class Setup{
     public $user;
     public $pass;
     public $db;
-    
+
     public function __construct($dbhost,$dbname,$dbuser,$dbpass){
         $this->connect = "mysql:host=" . $dbhost . ";dbname=" . $dbname . ";charsetutf8";
         $this->db = new PDO($this->connect, $dbuser, $dbpass);
@@ -14,17 +14,29 @@ class Setup{
     }
 
     public function registerUser($data){
-        $querry = "INSERT INTO `user` (`id`, `nama_user`, `email_user`, `pass_user`, `alamat_user`, `telp_user`) VALUES (NULL, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO `user` (`id`, `nama_user`, `email_user`, `pass_user`, `alamat_user`, `telp_user`) VALUES (NULL, ?, ?, ?, ?, ?)";
         $state = $this->db->prepare($querry);
         $parameter = [$data->nama, $data->email, $data->pass, $data->alamat, $data->telepon];
         return $state->execute($parameter);
     }
 
     public function tambahmakan($menu){
-        $querry = "INSERT INTO `menu` (`idmakanan`, `makanan_menu`, `jumlah_menu`,'alamat_menu, `pendonasi_menu`) VALUES (NULL, ?, ?, ?, ?)";
+        $query = "INSERT INTO `menu` (`idmakanan`, `makanan_menu`, `jumlah_menu`,'alamat_menu, `pendonasi_menu`) VALUES (NULL, ?, ?, ?, ?)";
         $state = $this->db->prepare($querry);
         $parameter = [$menu->makanan, $menu->jumlah, $menu->pendonasi];
         return $state->execute($parameter);
+    }
+
+    public static function getUser($id=NULL){
+        $setup = new Setup("localhost", "foodo", "root","");
+        $query = "SELECT * FROM 'user' ";
+        if($id!=NULL){
+            $query. = "WHERE id = ?";
+        }
+        $statement = $setup->db->prepare($query);
+        $parameters = [$id];
+        $statement ->execute($parameters);
+        return $statement->fetchAll();
     }
 }
 
--- prosestambah.php
+++ prosestambah.php
@@ -1,14 +1,15 @@
 <?php
-    require('tambahmakan.php')
-    require('setup.php')
-    $idmakanan = $_POST('IDmakanan')
-    $makanan = $_POST('makanan')
-    $jumlah = $_POST('jumlah')
-    $alamat = $_POST('alamat')
-    $pendonasi = $_POST('pendonasi')
+    require("tambahmakan.php");
+    require("setup.php");
+    $idmakanan = $_POST('IDmakanan');
+    $makanan = $_POST('makanan');
+    $jumlah = $_POST('jumlah');
+    $alamat = $_POST('alamat');
+    $pendonasi = $_POST('pendonasi');
 
     $menu = new menu($idmakanan, $makanan, $jumlah, $alamat, $pendonasi)
 
     $setdb = new Setup("localhost", "foodo", "root", "")
     $setdb = tambahmakan($menu)
+    
     ?>
